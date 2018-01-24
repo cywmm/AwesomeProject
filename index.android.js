e31e00fd3878bc2b58afdc13deb5acf443055919
wangming <cy_wangming@163.com>
2018-01-24 17:30:27 +0800

    update

--- index.android.js
+++ index.android.js
@@ -14,33 +14,34 @@ import MyScene from './demo/MyScene'
 class ListViewBasics extends Component {
     render() {
         return (
-            // <ListViewByTextView />
+            <ListViewByTextView/>
             // <ListViewByImge />
-            <Navigator
-                initialRoute={{title: '标题', index: 0}}
-                renderScene={(route, navigator) =>
-                    <MyScene
-                        title={route.title}
+            // <Navigator
+            //   initialRoute={{title: '标题', index: 0}}
+            //    renderScene={(route, navigator) =>
+            //        <MyScene
+            //          title={route.title}
 
-                        // Function to call when a new scene should be displayed
-                        onForward={ () => {
-                            const nextIndex = route.index + 1;
-                            navigator.push({
-                                title: '第' + nextIndex + '页',
-                                index: nextIndex,
-                            });
-                        }}
-
-                        // Function to call to go back to the previous scene
-                        onBack={() => {
-                            if (route.index > 0) {
-                                navigator.pop();
-                            }
-                        }}
-                    />
-                }
-            />
+            // Function to call when a new scene should be displayed
+            // onForward={ () => {
+            //     const nextIndex = route.index + 1;
+            //     navigator.push({
+            //         title: '第' + nextIndex + '页',
+            //         index: nextIndex,
+            //     });
+            // }}
+            //
+            // Function to call to go back to the previous scene
+            // onBack={() => {
+            //     if (route.index > 0) {
+            //         navigator.pop();
+            //     }
+            // }}
+            // />
+            // }
+            // />
         )
     }
 }
+
 AppRegistry.registerComponent('AwesomeProject', () => ListViewBasics);
